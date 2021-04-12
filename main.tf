39cf6d29798e6eb9422c7ec564bafe99b6777f5b
Zane Williamson <zwilliamson@flexport.com>
2021-04-12 14:31:56 -0600

    fixing ref to limit and request inputs

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
   }, var.kubernetes_resources_labels)
   daemonset_selector_labels = {
     "cookielab.io/application" = "metrics-server",
-    "cookielab.io/process" = "bootstrap"
+    "cookielab.io/process"     = "bootstrap"
   }
   daemonset_labels = merge(
     {
@@ -13,7 +13,7 @@ locals {
     var.kubernetes_resources_labels,
     {
       "cookielab.io/application" = "metrics-server",
-      "cookielab.io/process" = "bootstrap"
+      "cookielab.io/process"     = "bootstrap"
     }
   )
 }
@@ -28,15 +28,15 @@ resource "kubernetes_namespace" "datadog" {
 
 resource "kubernetes_service_account" "datadog_agent" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}datadog-agent"
+    name      = "${var.kubernetes_resources_name_prefix}datadog-agent"
     namespace = var.kubernetes_namespace
-    labels = local.kubernetes_resources_labels
+    labels    = local.kubernetes_resources_labels
   }
 }
 
 resource "kubernetes_cluster_role" "datadog_agent" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}datadog-agent"
+    name   = "${var.kubernetes_resources_name_prefix}datadog-agent"
     labels = local.kubernetes_resources_labels
   }
 
@@ -55,21 +55,21 @@ resource "kubernetes_cluster_role" "datadog_agent" {
 
   rule {
     api_groups = ["quota.openshift.io"]
-    resources = ["clusterresourcequotas"]
-    verbs = ["get", "list"]
+    resources  = ["clusterresourcequotas"]
+    verbs      = ["get", "list"]
   }
 
   rule {
-    api_groups = [""]
-    resources = ["configmaps"]
+    api_groups     = [""]
+    resources      = ["configmaps"]
     resource_names = ["datadogtoken", "datadog-leader-election"]
-    verbs = ["get", "update"]
+    verbs          = ["get", "update"]
   }
 
   rule {
     api_groups = [""]
-    resources = ["configmaps"]
-    verbs = ["create"]
+    resources  = ["configmaps"]
+    verbs      = ["create"]
   }
 
   rule {
@@ -95,28 +95,28 @@ resource "kubernetes_cluster_role" "datadog_agent" {
 
 resource "kubernetes_cluster_role_binding" "datadog_agent" {
   metadata {
-    name = kubernetes_cluster_role.datadog_agent.metadata.0.name
+    name   = kubernetes_cluster_role.datadog_agent.metadata.0.name
     labels = kubernetes_cluster_role.datadog_agent.metadata.0.labels
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind = "ClusterRole"
-    name = kubernetes_cluster_role.datadog_agent.metadata.0.name
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.datadog_agent.metadata.0.name
   }
 
   subject {
-    kind = "ServiceAccount"
+    kind      = "ServiceAccount"
     namespace = kubernetes_service_account.datadog_agent.metadata.0.namespace
-    name = kubernetes_service_account.datadog_agent.metadata.0.name
+    name      = kubernetes_service_account.datadog_agent.metadata.0.name
   }
 }
 
 resource "kubernetes_secret" "datadog_agent" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}datadog-secret"
+    name      = "${var.kubernetes_resources_name_prefix}datadog-secret"
     namespace = var.kubernetes_namespace
-    labels = local.daemonset_labels
+    labels    = local.daemonset_labels
   }
 
   data = {
@@ -126,9 +126,9 @@ resource "kubernetes_secret" "datadog_agent" {
 
 resource "kubernetes_daemonset" "datadog_agent" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}datadog-agent"
+    name      = "${var.kubernetes_resources_name_prefix}datadog-agent"
     namespace = var.kubernetes_namespace
-    labels = local.daemonset_labels
+    labels    = local.daemonset_labels
   }
 
   spec {
@@ -138,29 +138,29 @@ resource "kubernetes_daemonset" "datadog_agent" {
 
     template {
       metadata {
-        name = "datadog-agent"
+        name   = "datadog-agent"
         labels = local.daemonset_selector_labels
       }
 
       spec {
-        service_account_name = kubernetes_service_account.datadog_agent.metadata.0.name
+        service_account_name            = kubernetes_service_account.datadog_agent.metadata.0.name
         automount_service_account_token = true
 
         container {
-          image = "${var.datadog_agent_image}:${var.datadog_agent_image_tag}"
+          image             = "${var.datadog_agent_image}:${var.datadog_agent_image_tag}"
           image_pull_policy = "Always"
-          name = "datadog-agent"
+          name              = "datadog-agent"
 
           port {
             container_port = 8125
-            name = "dogstatsdport"
-            protocol = "UDP"
+            name           = "dogstatsdport"
+            protocol       = "UDP"
           }
 
           port {
             container_port = 8126
-            name = "traceport"
-            protocol = "TCP"
+            name           = "traceport"
+            protocol       = "TCP"
           }
 
           env {
@@ -168,43 +168,43 @@ resource "kubernetes_daemonset" "datadog_agent" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.datadog_agent.metadata.0.name
-                key = "api-key"
+                key  = "api-key"
               }
             }
           }
 
           env {
-            name = "DD_SITE"
+            name  = "DD_SITE"
             value = var.datadog_agent_site
           }
 
           env {
-            name = "KUBERNETES"
+            name  = "KUBERNETES"
             value = "true"
           }
 
           env {
-            name = "DD_DOGSTATSD_NON_LOCAL_TRAFFIC"
+            name  = "DD_DOGSTATSD_NON_LOCAL_TRAFFIC"
             value = "false"
           }
 
           env {
-            name = "DD_HEALTH_PORT"
+            name  = "DD_HEALTH_PORT"
             value = "5555"
           }
 
           env {
-            name = "DD_COLLECT_KUBERNETES_EVENTS"
+            name  = "DD_COLLECT_KUBERNETES_EVENTS"
             value = var.datadog_agent_options_collect_kubernetes_events
           }
 
           env {
-            name = "DD_LEADER_ELECTION"
+            name  = "DD_LEADER_ELECTION"
             value = "true"
           }
 
           env {
-            name = "DD_APM_ENABLED"
+            name  = "DD_APM_ENABLED"
             value = var.datadog_agent_options_apm_enabled
           }
 
@@ -218,51 +218,51 @@ resource "kubernetes_daemonset" "datadog_agent" {
           }
 
           env {
-            name = "DD_KUBELET_TLS_VERIFY"
+            name  = "DD_KUBELET_TLS_VERIFY"
             value = var.datadog_agent_options_kubelet_tls_verify
           }
 
           resources {
-            requests {
+            requests = {
               memory = "256Mi"
-              cpu = "200m"
+              cpu    = "200m"
             }
 
-            limits {
+            limits = {
               memory = "256Mi"
-              cpu = "200m"
+              cpu    = "200m"
             }
           }
 
           volume_mount {
-            name = "dockersocket"
+            name       = "dockersocket"
             mount_path = "/var/run/docker.sock"
           }
 
           volume_mount {
-            name = "procdir"
+            name       = "procdir"
             mount_path = "/host/proc"
-            read_only = true
+            read_only  = true
           }
 
           volume_mount {
-            name = "cgroups"
+            name       = "cgroups"
             mount_path = "/host/sys/fs/cgroup"
-            read_only = true
+            read_only  = true
           }
 
           volume_mount {
-            name = "s6-run"
+            name       = "s6-run"
             mount_path = "/var/run/s6"
           }
 
           volume_mount {
-            name = "logpodpath"
+            name       = "logpodpath"
             mount_path = "/var/log/pods"
           }
 
           volume_mount {
-            name = "logcontainerpath"
+            name       = "logcontainerpath"
             mount_path = "/var/lib/docker/containers"
           }
 
@@ -273,10 +273,10 @@ resource "kubernetes_daemonset" "datadog_agent" {
             }
 
             initial_delay_seconds = 15
-            period_seconds = 15
-            timeout_seconds = 5
-            success_threshold = 1
-            failure_threshold = 3
+            period_seconds        = 15
+            timeout_seconds       = 5
+            success_threshold     = 1
+            failure_threshold     = 3
           }
         }
 
@@ -323,7 +323,7 @@ resource "kubernetes_daemonset" "datadog_agent" {
         }
 
         toleration {
-          key = "node-role.kubernetes.io/master"
+          key    = "node-role.kubernetes.io/master"
           effect = "NoSchedule"
         }
       }
